0832612bef1c6bbe28a89f95516982a55844e02b
AlesiaZuk <78986532+AlesiaZuk@users.noreply.github.com>
2021-11-07 23:25:23 +0200

    Пофиксила компоненты-классы на функции

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,23 +1,19 @@
-import { Component } from "react";
 import PropTypes from "prop-types";
 import s from "./FeedbackOptions.module.css";
 
-class FeedbackOptions extends Component {
-  render() {
-    const { options, onLeaveFeedback } = this.props;
-    return (
-      <>
-        <button
-          className={s.button}
-          type="button"
-          name={options}
-          onClick={onLeaveFeedback}
-        >
-          {options}
-        </button>
-      </>
-    );
-  }
+function FeedbackOptions({ options, onLeaveFeedback }) {
+  return (
+    <>
+      <button
+        className={s.button}
+        type="button"
+        name={options}
+        onClick={onLeaveFeedback}
+      >
+        {options}
+      </button>
+    </>
+  );
 }
 
 FeedbackOptions.propTypes = {
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,23 +1,19 @@
-import { Component } from "react";
 import PropTypes from "prop-types";
 import s from "./Statistics.module.css";
 
-class Statistics extends Component {
-  render() {
-    const { good, neutral, bad, total, positivePercentage } = this.props;
-    return (
-      <>
-        <p className={s.text}>Good:{good}</p>
-        <p className={s.text}>Neutral:{neutral}</p>
-        <p className={s.text}>Bad:{bad}</p>
-        <p className={s.text}>Total:{total}</p>
-        <p className={s.text}>
-          Positive feedback:
-          {positivePercentage}%
-        </p>
-      </>
-    );
-  }
+function Statistics({ good, neutral, bad, total, positivePercentage }) {
+  return (
+    <>
+      <p className={s.text}>Good:{good}</p>
+      <p className={s.text}>Neutral:{neutral}</p>
+      <p className={s.text}>Bad:{bad}</p>
+      <p className={s.text}>Total:{total}</p>
+      <p className={s.text}>
+        Positive feedback:
+        {positivePercentage}%
+      </p>
+    </>
+  );
 }
 
 Statistics.propTypes = {
